a418d48465491c8d8e5c8d13fa7b9bce21112ab0
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-10-27 14:27:13 +0300

    create server

---
+++ server/data.js
@@ -0,0 +1,16 @@
+const data = [
+  {
+    nickname: "Superman",
+    real_name: "Clark Kent",
+    origin_description:
+      "he was born Kal-El on the planet Krypton, before being rocketed to Earth as an infant by his scientist father Jor-El, moments before Krypton's destruction...",
+    superpowers:
+      "solar energy absorption and healing factor, solar flare and heat vision, solar invulnerability, flight...",
+    catch_phrase:
+      "“Look, up in the sky, it's a bird, it's a plane, it's Superman!”",
+
+    images: "https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png",
+  },
+];
+
+module.exports = data;
