e5554dfa9bc5e3ebacb0be3a52a65b06fe047de5
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-10-30 19:38:28 +0200

    add store test

--- client/src/store/superheroes/reducer.test.js
+++ client/src/store/superheroes/reducer.test.js
@@ -5,115 +5,192 @@ describe("product reducer", () => {
     expect(
       reducer(
         {
-          categorieProductList: [],
-          searchWord: "",
-          isLoading: false,
-          hasError: false,
-          isFilterOpenMobile: false,
-          productsQuntity: 0,
-          filterOnChange: { isLoading: false, productsQuntityOnChange: null },
+          isLoaded: false,
+          error: null,
+          data: [],
+          quantity: null,
         },
         {}
       )
     ).toEqual({
-      categorieProductList: [],
-      searchWord: "",
-      isLoading: false,
-      hasError: false,
-      isFilterOpenMobile: false,
-      productsQuntity: 0,
-      filterOnChange: { isLoading: false, productsQuntityOnChange: null },
+      isLoaded: false,
+      error: null,
+      data: [],
+      quantity: null,
     });
   });
 
-  describe("for START_FETCH_PRODUCTS action", () => {
-    it("should show loading...", () => {
+  describe("CRUD action with superhero", () => {
+    it("to GET_DATA_REQUEST action", () => {
       expect(
         reducer(
           {
-            categorieProductList: [],
-            searchWord: "",
-            isLoading: false,
-            hasError: false,
-            isFilterOpenMobile: false,
-            productsQuntity: 0,
-            filterOnChange: { isLoading: false, productsQuntityOnChange: null },
+            isLoaded: true,
+            error: null,
+            data: [],
+            quantity: null,
           },
           {
-            type: "START_FETCH_PRODUCTS",
+            type: "GET_DATA_REQUEST",
+            payload: true,
           }
         )
       ).toEqual({
-        categorieProductList: [],
-        searchWord: "",
-        isLoading: true,
-        hasError: false,
-        isFilterOpenMobile: false,
-        productsQuntity: 0,
-        filterOnChange: { isLoading: false, productsQuntityOnChange: null },
+        isLoaded: true,
+        error: null,
+        data: [],
+        quantity: null,
       });
     });
-  });
-
-  describe("for LOADED_PRODUCTS action", () => {
-    it("should add products", () => {
+    it("to GET_DATA_ERROR action", () => {
       expect(
         reducer(
           {
-            categorieProductList: [
-              { name: "asus", title: "laptop 1", vendorCode: 111111 },
-            ], //in future
-            searchWord: "",
-            isLoading: false,
-            hasError: false,
-            isFilterOpenMobile: false,
-            productsQuntity: 0,
-            filterOnChange: { isLoading: false, productsQuntityOnChange: null },
+            isLoaded: false,
+            error: { message: "Error on server" },
+            data: [],
+            quantity: null,
           },
           {
-            type: "LOADED_PRODUCTS",
-            payload: [{ name: "asus", title: "laptop 1", vendorCode: 111111 }],
+            type: "GET_DATA_ERROR",
+            payload: { message: "Error on server" },
           }
         )
       ).toEqual({
-        categorieProductList: [
-          { name: "asus", title: "laptop 1", vendorCode: 111111 },
+        isLoaded: false,
+        error: { message: "Error on server" },
+        data: [],
+        quantity: null,
+      });
+    });
+    it("to GET_DATA_SUCCESS action", () => {
+      expect(
+        reducer(
+          {
+            isLoaded: true,
+            error: null,
+            data: [
+              {
+                _id: "635c179f6326e15c833b3e0f",
+                nickname: "Supermagddd",
+                images:
+                  "https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png",
+              },
+            ],
+            quantity: 1,
+          },
+          {
+            type: "GET_DATA_SUCCESS",
+            payload: {
+              data: [
+                {
+                  _id: "635c179f6326e15c833b3e0f",
+                  nickname: "Supermagddd",
+                  images:
+                    "https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png",
+                },
+              ],
+              quantity: 1,
+            },
+          }
+        )
+      ).toEqual({
+        isLoaded: true,
+        error: null,
+        data: [
+          {
+            _id: "635c179f6326e15c833b3e0f",
+            nickname: "Supermagddd",
+            images:
+              "https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png",
+          },
         ],
-        searchWord: "",
-        isLoading: false,
-        hasError: false,
-        isFilterOpenMobile: false,
-        productsQuntity: 0,
-        filterOnChange: { isLoading: false, productsQuntityOnChange: null },
+        quantity: 1,
       });
     });
-  });
-
-  describe("for ERROR_LOADED_PRODUCTS action", () => {
-    it("should show error", () => {
+    it("to DELETE_DATA action", () => {
       expect(
         reducer(
           {
-            categorieProductList: [],
-            searchWord: "",
-            isLoading: false,
-            hasError: false,
-            isFilterOpenMobile: false,
-            productsQuntity: 0,
-            filterOnChange: { isLoading: false, productsQuntityOnChange: null },
+            isLoaded: true,
+            error: null,
+            data: [],
+            quantity: null,
           },
           {
-            type: "ERROR_LOADED_PRODUCTS",
+            type: "DELETE_DATA",
+            payload: {
+              _id: "635c179f6326e15c833b3e0f",
+              nickname: "Supermagddd",
+              images:
+                "https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png",
+            },
           }
         )
       ).toEqual({
-        categorieProductList: [],
-        searchWord: "",
-        isLoading: false,
-        hasError: true,
-        isFilterOpenMobile: false,
-        productsQuntity: 0,
-        filterOnChange: { isLoading: false, productsQuntityOnChange: null },
+        isLoaded: true,
+        error: null,
+        data: [],
+        quantity: null,
+      });
+    });
+
+    it("to EDIT_DATA action", () => {
+      expect(
+        reducer(
+          {
+            isLoaded: true,
+            error: null,
+            data: [
+              {
+                nickname: "Supermagdd",
+                real_name: "Clark Kent",
+                origin_description:
+                  "he was born Kal-El on the planet Krypton, before being rocketed to Earth as an infant by his scientist father Jor-El, moments before Krypton's destruction...",
+                superpowers:
+                  "solar energy absorption and healing factor, solar flare and heat vision, solar invulnerability, flight...",
+                catch_phrase:
+                  "“Look, up in the sky, it's a bird, it's a plane, it's Superman!”",
+                images:
+                  "https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png",
+              },
+            ],
+            quantity: 1,
+          },
+          {
+            type: "EDIT_DATA",
+            payload: {
+              nickname: "Supermagdd",
+              real_name: "Clark Kent",
+              origin_description:
+                "he was born Kal-El on the planet Krypton, before being rocketed to Earth as an infant by his scientist father Jor-El, moments before Krypton's destruction...",
+              superpowers:
+                "solar energy absorption and healing factor, solar flare and heat vision, solar invulnerability, flight...",
+              catch_phrase:
+                "“Look, up in the sky, it's a bird, it's a plane, it's Superman!”",
+              images:
+                "https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png",
+            },
+          }
+        )
+      ).toEqual({
+        isLoaded: true,
+        error: null,
+        data: [
+          {
+            nickname: "Supermagdd",
+            real_name: "Clark Kent",
+            origin_description:
+              "he was born Kal-El on the planet Krypton, before being rocketed to Earth as an infant by his scientist father Jor-El, moments before Krypton's destruction...",
+            superpowers:
+              "solar energy absorption and healing factor, solar flare and heat vision, solar invulnerability, flight...",
+            catch_phrase:
+              "“Look, up in the sky, it's a bird, it's a plane, it's Superman!”",
+            images:
+              "https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png",
+          },
+        ],
+        quantity: 1,
       });
     });
   });
